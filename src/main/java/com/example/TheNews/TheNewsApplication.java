5dc8ae5278fbeb12b49686700aa0d2cf9bfa7dba
ynadvodnyuk <rimsky.yana@yandex.com>
2023-11-21 16:46:53 +0300

    git testing

--- src/main/java/com/example/TheNews/TheNewsApplication.java
+++ src/main/java/com/example/TheNews/TheNewsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheNewsApplication {
+	//проверяю гит
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheNewsApplication.class, args);
