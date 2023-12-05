94aaf911ecd89e5f2c0bc3b77cc914d6e56fa304
ynadvodnyuk <rimsky.yana@yandex.com>
2023-12-05 15:21:16 +0300

    tables

---
+++ src/main/java/com/example/TheNews/Comment.java
@@ -0,0 +1,5 @@
+package com.example.TheNews;
+
+public class Comment {
+    
+}
---
+++ src/main/java/com/example/TheNews/User.java
@@ -0,0 +1,25 @@
+package com.example.TheNews;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long user_id;
+
+	@Column(name = "first_name")
+	private String first_name;
+
+	@Column(name = "last_name")
+	private String last_name;
+
+    @Column(name = "role")
+	private String role;
+
+	@Column(name = "username")
+	private String username;
+
+    @Column(name = "password")
+	private String password;
+}
