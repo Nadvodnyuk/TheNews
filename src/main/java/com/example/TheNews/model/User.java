5d4f626fa51988bd1ea87718041fb2397932920a
ynadvodnyuk <rimsky.yana@yandex.com>
2024-01-26 12:02:12 +0300

    напиши код для пропертиз

--- src/main/java/com/example/TheNews/model/User.java
+++ src/main/java/com/example/TheNews/model/User.java
@@ -11,15 +11,15 @@ public class User {
 	private String last_name;
 	private String role;
 	private String username;
-	// private List<Like> likes;
-	// private List<Article> articles;
-	// private List<Comment> comments;
+//	 private List<Like> likes;
+//	 private List<Article> articles;
+//	 private List<Comment> comments;
 
     public static User toModel(UserEntity entity) {
         User model = new User();
-        model.setId(entity.getId());
-        model.setFirstName(entity.getFirstName());
-        model.setLastName(entity.getLastName());
+        model.setUser_id(entity.getId());
+        model.setFirst_name(entity.getFirstName());
+        model.setLast_name(entity.getLastName());
         model.setRole(entity.getRole());
         model.setUsername(entity.getUsername());
         // model.setTodos(entity.getTodos().stream().map(Todo::toModel).collect(Collectors.toList()));
@@ -31,40 +31,40 @@ public class User {
     public User() {
     }
 
-    public Long getId() {
+    public long getUser_id() {
         return user_id;
     }
 
-    public String getFirstName() {
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFirst_name() {
         return first_name;
     }
 
-    public String getLastName() {
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
         return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 
     public String getRole() {
         return role;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setId(Long user_id) {
-        this.user_id = user_id;
-    }
-
-    public void setFirstName(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public void setLastName(String last_name) {
-        this.last_name = last_name;
-    }
-
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
