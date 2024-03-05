8c736b4d060a35d060245cd4b05f2336692a86a9
ynadvodnyuk <rimsky.yana@yandex.com>
2024-03-05 11:12:55 +0300

    imports and readme

--- TheNews-frontend/src/main.js
+++ TheNews-frontend/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import { createWebHistory } from 'vue-router'
+import createRouter from './pages/routes.js'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+const router = createRouter(createWebHistory())
+const app = createApp(App)
+const store = createPinia()
 
-createApp(App).mount('#app')
+app.use(router).use(Antd).use(store).mount('#app')
+
