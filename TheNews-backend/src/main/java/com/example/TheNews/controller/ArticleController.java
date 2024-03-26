3665b754ea3269b2c51539af03d6b59d7600dedb
Mark Revazov <revazov.m@mail.ru>
2024-03-26 13:01:40 +0300

    CHISTKA MUSORA

--- TheNews-backend/src/main/java/com/example/TheNews/controller/ArticleController.java
+++ TheNews-backend/src/main/java/com/example/TheNews/controller/ArticleController.java
@@ -1,7 +1,6 @@
 package com.example.TheNews.controller;
 
 import com.example.TheNews.entity.ArticleEntity;
-import com.example.TheNews.dto.ArticleDtoOld;
 import com.example.TheNews.exception.NotFoundException;
 import com.example.TheNews.service.ArticleService;
 import org.modelmapper.ModelMapper;
@@ -17,34 +16,6 @@ public class ArticleController {
     private ModelMapper modelMapper;
     @Autowired
     private ArticleService articleService;
-
-
-    @PostMapping
-    public ResponseEntity createArticle(@RequestBody ArticleEntity art,
-                                        @RequestParam Long user_id,
-                                        @RequestBody ArticleDtoOld articleDto) {
-        try {
-            ArticleEntity articleRequest = modelMapper.map(articleDto, ArticleEntity.class);
-            ArticleDtoOld articleResponse = modelMapper.map(
-                    articleService.createArticle(art, user_id), ArticleDtoOld.class);
-
-            return ResponseEntity.ok(articleResponse);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Произошла ошибка, статья" + e.getMessage());
-        }
-    }
-
-    @GetMapping
-    public ResponseEntity getArtByUserId(@RequestParam Long user_id) {
-        try {
-            ArticleDtoOld articleResponse = modelMapper.map(articleService.getOne(user_id), ArticleDtoOld.class);
-            return ResponseEntity.ok(articleResponse);
-        } catch (NotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Произошла ошибка, статья");
-        }
-    }
 
     @DeleteMapping("/{user_id}")
     public ResponseEntity deleteArticle(@PathVariable Long user_id) {
